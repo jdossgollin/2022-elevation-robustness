c66b9b2135ebec94b3579e6ba58121657a9c5a0b
James Doss-Gollin <no@mail>
2021-04-19 22:11:27 -0400

    improved priors lead to good fits

--- scripts/surge_prior_checks.jl
+++ scripts/surge_prior_checks.jl
@@ -8,21 +8,32 @@ using DrWatson
 using Turing, Distributions, DynamicHMC
 
 using Unitful
-using Plots, StatsPlots
+using Plots, StatsPlots, ColorSchemes
 using NorfolkFloods
 
 priors_on_rl = [
-    (0.9, Normal(5, 2)), # 10 year flood
-    (0.99, Normal(7, 3)), # 100 year flood
-    (0.999, Normal(10, 4)), # 1000 year flood
+    (0.9, LogNormal(1.75, 0.5)), # 10 year flood
+    (0.99, LogNormal(2, 0.5)), # 100 year flood
+    (0.999, LogNormal(2.25, 0.5)), # 1000 year flood
 ]
+p = plot(
+    xlims = (0, 30),
+    xlabel = "Storm Surge (ft)",
+    ylabel = "",
+    title = "Prior on Return Levels",
+)
+for (pr, d) in priors_on_rl
+    plot!(p, d, label = "$(Int(round(1 / (1 - pr)))) Year Flood")
+end
+p
+savefig(p, plotsdir("priors_on_surge_quantiles.pdf"))
 
 """
 The full GEV model for storm surge:
 
-Assume that the GEV process is stationary. Instead of applying priors over the parameters, use priors over quantiles of the outcome distribution. See Coles & Tawn (1996) and Stephenson (2015). This can be implemented more straightforwardly in Turing.
+Assume that the GEV process is stationary. Instead of applying priors over the parameters, use priors over quantiles of the outcome distribution. See Coles & Tawn (1996) and Stephenson (2015) for related efforts.
 
-The specific values of (α, θ) are given above
+Here we constrain ξ >= 0, which requires y to have a lower bound, and  and μ >= 0, which requires that bound to be at least 0. This is reasonable because we know that y >= 0 as the surges are the maxima of residuals, and the residuals have mean zero (by construction; we have subtracted the mean sea level for the year).
 
 Coles, S. G., & Tawn, J. A. (1996). A Bayesian analysis of extreme rainfall data. Journal of the Royal Statistical Society: Series C (Applied Statistics), 45(4), 463–478. https://doi.org/10.2307/2986068
 
@@ -31,7 +42,7 @@ Stephenson, A. (2015). Bayesian inference for extreme value modeling. In D. K. D
 @model function StationaryGEV_quantile_priors(y)
 
     # weak priors
-    μ ~ Flat()
+    μ ~ FlatPos(0.0)
     σ ~ FlatPos(0.0)
     ξ ~ FlatPos(0.0)
 
@@ -54,38 +65,38 @@ end
 prior_model = StationaryGEV_quantile_priors([missing])
 
 # draw samples from the prior
-prior = sample(prior_model, DynamicNUTS(), 10_000; drop_warmup = true)
+prior = sample(prior_model, DynamicNUTS(), 50_000; drop_warmup = true)
 plot(prior, [:μ, :σ, :ξ])
 yhat_prior = rand.(GeneralizedExtremeValue.(prior[:μ], prior[:σ], prior[:ξ]))[:];
-for (prob, prior_dist) in priors_on_rl
-    obs = quantile(yhat_prior, prob)
-    desired = prior_dist
-    @show prob, obs, desired
-end
 
 # use historical data for posterior
 obs = get_norfolk_annual()
 y = ustrip.(u"ft", obs.surge) # observed data, in feet
 model = StationaryGEV_quantile_priors(y)
-posterior = sample(model, DynamicNUTS(), 10_000, drop_warmup = true)
+posterior = sample(model, DynamicNUTS(), 50_000, drop_warmup = true)
 summarystats(posterior)
 plot(posterior)
 
 # posterior predictive distribution
 yhat = rand.(GeneralizedExtremeValue.(posterior[:μ], posterior[:σ], posterior[:ξ]))[:];
 
-p1 = plot(obs.year, y, label = "", ylabel = "Storm Surge (ft)")
+colors = ColorSchemes.tab10
 
+p1 = plot(obs.year, y, label = "", ylabel = "Storm Surge (ft)", title="Observations", marker=".")
 p2 = histogram(
     yhat,
-    label = "Posterior",
     xlabel = "",
     ylabel = "",
+    label = "Posterior",
+    title="Distribution",
     normalize = :pdf,
     orientation = :horizontal,
     linecolor = false,
     fillalpha = 0.5,
+    color = colors[1],
+    legend = :bottomright,
 )
+hline!(p2, [quantile(yhat, 0.99)], color = colors[1], label = "")
 histogram!(
     p2,
     yhat_prior,
@@ -94,5 +105,9 @@ histogram!(
     fillalpha = 0.5,
     orientation = :horizontal,
     normalize = :pdf,
+    color = colors[2],
 )
-plot(p1, p2, link = :y, yrange = (-2.5, 12.5), layout = (1, 2))
+hline!(p2, [quantile(yhat_prior, 0.99)], color = colors[2], label = "")
+pp = plot(p1, p2, link = :y, yrange = (-1.25, 10), layout = (1, 2))
+pp
+savefig(pp, plotsdir("surge_prior_posterior_dists.pdf"))
